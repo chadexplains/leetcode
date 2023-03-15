96b3619d7d05d69374dce84cea6cfd6efbb9daf7
An Engineer Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-15 14:50:23 -0700

    Upload brute force code

---
+++ 26/brute_force.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            j = i + 1
+            while j < len(nums):
+                if nums[i] == nums[j]:
+                    nums.pop(j)
+                else:
+                    j += 1
+            i += 1
+        return len(nums)
