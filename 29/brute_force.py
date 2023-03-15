b697458b0334a6970d3b8e93b0f2fe4d01759640
An Engineer Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-15 14:50:27 -0700

    Upload brute force code

---
+++ 29/brute_force.py
@@ -0,0 +1,11 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+        sign = -1 if (dividend < 0) ^ (divisor < 0) else 1
+        dividend, divisor = abs(dividend), abs(divisor)
+        quotient = 0
+        while dividend >= divisor:
+            dividend -= divisor
+            quotient += 1
+        return sign * quotient
