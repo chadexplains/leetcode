e254f4e418d67da07a83d4a5e22e518408e9fe81
An Engineer Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-20 15:01:16 -0700

    Upload code

---
+++ 179/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [str(num) for num in nums]
+        nums.sort(key=lambda x: x*10, reverse=True)
+        return str(int(''.join(nums)))
