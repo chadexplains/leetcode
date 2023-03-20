10d15a4903feb1165405f0f171df4d6a2276aee3
An Engineer Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-20 15:00:36 -0700

    Upload code

---
+++ 82/solution.py
@@ -0,0 +1,14 @@
+def deleteDuplicates(head):
+    dummy = ListNode(0)
+    dummy.next = head
+    prev = dummy
+    curr = head
+    while curr:
+        if curr.next and curr.val == curr.next.val:
+            while curr.next and curr.val == curr.next.val:
+                curr = curr.next
+            prev.next = curr.next
+        else:
+            prev = prev.next
+        curr = curr.next
+    return dummy.next
