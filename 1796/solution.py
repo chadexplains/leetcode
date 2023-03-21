59238041387ab379b4e5999c6b3af397def5ead4
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:43:20 -0700

    Upload code

---
+++ 1796/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        largest = second_largest = -1
+        for c in s:
+            if c.isdigit():
+                digit = int(c)
+                if digit > largest:
+                    second_largest = largest
+                    largest = digit
+                elif digit != largest and digit > second_largest:
+                    second_largest = digit
+        return second_largest
