00a55734d40dc45d61c7ee8ef225ca55bdfe7bea
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 08:49:10 -0700

    Upload code

---
+++ 594/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        freq = collections.Counter(nums)
+        max_len = 0
+        for num in freq:
+            if num + 1 in freq:
+                max_len = max(max_len, freq[num] + freq[num + 1])
+        return max_len
