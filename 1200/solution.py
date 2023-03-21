2b589918501e90cc9aed8bfd3a40d4d4606eba3a
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:30:01 -0700

    Upload code

---
+++ 1200/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        min_diff = float('inf')
+        result = []
+        for i in range(len(arr)-1):
+            diff = abs(arr[i+1] - arr[i])
+            if diff < min_diff:
+                min_diff = diff
+                result = [[arr[i], arr[i+1]]]
+            elif diff == min_diff:
+                result.append([arr[i], arr[i+1]])
+        return result
