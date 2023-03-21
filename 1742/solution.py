8a0b80938cb0e1f634f7610c0ac35f585d7d90b6
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:42:06 -0700

    Upload code

---
+++ 1742/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSumAfterOperation(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = [0] * n
+        right = [0] * n
+        left[0] = nums[0]
+        right[n-1] = nums[n-1]
+        for i in range(1, n):
+            left[i] = max(nums[i], left[i-1] + nums[i])
+        for i in range(n-2, -1, -1):
+            right[i] = max(nums[i], right[i+1] + nums[i])
+        res = max(left)
+        for i in range(1, n):
+            res = max(res, left[i-1] + right[i])
+        return res
