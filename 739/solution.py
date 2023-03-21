90cfa454f0792c29cd485e4deca20075c8899dfe
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 08:52:21 -0700

    Upload code

---
+++ 739/solution.py
@@ -0,0 +1,10 @@
+def dailyTemperatures(T):
+    stack = []
+    result = [0] * len(T)
+    for i in range(len(T) - 1, -1, -1):
+        while stack and T[i] >= T[stack[-1]]:
+            stack.pop()
+        if stack:
+            result[i] = stack[-1] - i
+        stack.append(i)
+    return result
