5afe18d0d68975a8554c4c5e33dbd180cc789f95
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 08:53:56 -0700

    Upload code

---
+++ 807/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        row_max = [max(row) for row in grid]
+        col_max = [max(col) for col in zip(*grid)]
+        res = 0
+        for i in range(n):
+            for j in range(n):
+                res += min(row_max[i], col_max[j]) - grid[i][j]
+        return res
