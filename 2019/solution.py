614a292074706f76affdeabb975702ce6a5732d9
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:48:19 -0700

    Upload code

---
+++ 2019/solution.py
@@ -0,0 +1,11 @@
+def removeNthFromEnd(head, n):
+    dummy = ListNode(0)
+    dummy.next = head
+    slow = fast = dummy
+    for i in range(n):
+        fast = fast.next
+    while fast.next:
+        slow = slow.next
+        fast = fast.next
+    slow.next = slow.next.next
+    return dummy.next
