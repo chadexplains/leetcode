f1ee60b5ffa91da4aa19ada6357a7061fae5d988
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 08:46:26 -0700

    Upload code

---
+++ 475/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        i = r = 0
+        for house in houses:
+            while i < len(heaters) - 1 and abs(heaters[i + 1] - house) <= abs(heaters[i] - house):
+                i += 1
+            r = max(r, abs(heaters[i] - house))
+        return r
