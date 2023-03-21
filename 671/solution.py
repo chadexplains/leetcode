0889958280ee8efafaec5573b9f709607ef31cbc
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 08:50:53 -0700

    Upload code

---
+++ 671/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        def dfs(node):
+            if not node:
+                return -1
+            if node.val > root.val:
+                return node.val
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if left == -1:
+                return right
+            if right == -1:
+                return left
+            return min(left, right)
+        return dfs(root)
