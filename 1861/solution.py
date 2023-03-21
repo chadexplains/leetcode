9ffe357ab3cc03f108f4c6d68809705a26e2dcf9
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:44:44 -0700

    Upload code

---
+++ 1861/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numOfPairs(self, nums: List[str], target: str) -> int:
+        freq = collections.Counter(nums)
+        count = 0
+        used = set()
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j and (i, j) not in used and (j, i) not in used and nums[i] + nums[j] == target:
+                    count += 1
+                    used.add((i, j))
+        return count
