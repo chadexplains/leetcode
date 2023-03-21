5d117a1e6835b79ddc67612346fb9b7f90e8471e
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:31:09 -0700

    Upload code

---
+++ 1249/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        remove = set()
+        for i, c in enumerate(s):
+            if c == '(': stack.append(i)
+            elif c == ')':
+                if stack: stack.pop()
+                else: remove.add(i)
+        remove = remove.union(set(stack))
+        return ''.join([c for i, c in enumerate(s) if i not in remove])
