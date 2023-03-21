ac7627379913398f16473d05a3ce9e731c60e52b
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:48:22 -0700

    Upload code

---
+++ 2022/solution.py
@@ -0,0 +1,10 @@
+def construct2DArray(original: List[int], m: int, n: int) -> List[List[int]]:
+    if len(original) != m * n:
+        return []
+    res = []
+    for i in range(m):
+        row = []
+        for j in range(n):
+            row.append(original[i * n + j])
+        res.append(row)
+    return res
