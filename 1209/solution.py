72a466382f876677aad5f9f016e854b100f619e3
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:30:12 -0700

    Upload code

---
+++ 1209/solution.py
@@ -0,0 +1,10 @@
+def removeDuplicates(s: str, k: int) -> str:
+    stack = []
+    for c in s:
+        if stack and stack[-1][0] == c:
+            stack[-1][1] += 1
+            if stack[-1][1] == k:
+                stack.pop()
+        else:
+            stack.append([c, 1])
+    return ''.join(c * k for c, k in stack)
