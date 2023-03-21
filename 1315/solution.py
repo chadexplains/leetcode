9c4c5d0da17591dd4f32831f16db14fd8334467d
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:32:37 -0700

    Upload code

---
+++ 1315/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        def dfs(node, parent, grandparent):
+            if not node:
+                return 0
+            total = 0
+            if grandparent and grandparent.val % 2 == 0:
+                total += node.val
+            total += dfs(node.left, node, parent)
+            total += dfs(node.right, node, parent)
+            return total
+        return dfs(root, None, None)
