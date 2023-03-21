099fa78cf64969844e739b97bcbddb1dcc643947
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:32:35 -0700

    Upload code

---
+++ 1313/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        decompressed = []
+        for i in range(0, len(nums), 2):
+            freq = nums[i]
+            val = nums[i+1]
+            decompressed += [val] * freq
+        return decompressed
