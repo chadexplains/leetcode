17c3018c576a6c1cbda5c7a401a8f17d43671329
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:32:16 -0700

    Upload code

---
+++ 1296/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        freq = collections.Counter(nums)
+        nums.sort()
+        for num in nums:
+            if freq[num] > 0:
+                for i in range(num, num+k):
+                    if freq[i] == 0:
+                        return False
+                    freq[i] -= 1
+        return True
