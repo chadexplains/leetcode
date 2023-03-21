966541e80e510177b892f088b574840a951a8ba0
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:26:44 -0700

    Upload code

---
+++ 1048/solution.py
@@ -0,0 +1,10 @@
+def longestStrChain(words):
+    words.sort(key=len)
+    dp = {}
+    for word in words:
+        dp[word] = 1
+        for i in range(len(word)):
+            predecessor = word[:i] + word[i+1:]
+            if predecessor in dp:
+                dp[word] = max(dp[word], dp[predecessor] + 1)
+    return max(dp.values())
