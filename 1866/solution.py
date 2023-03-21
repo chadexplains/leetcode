1501e55868b4b0433ff56f9af82d0aea92eec7c4
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:44:51 -0700

    Upload code

---
+++ 1866/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rearrangeSticks(self, n: int, k: int) -> int:
+        dp = [[0] * (k+1) for _ in range(n+1)]
+        dp[0][0] = 1
+        for i in range(1, n+1):
+            for j in range(1, k+1):
+                dp[i][j] = dp[i-1][j-1] + (i-1) * dp[i-1][j]
+        return dp[n][k] % (10**9 + 7)
