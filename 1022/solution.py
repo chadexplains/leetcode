a8ed13a8d9150c993f31a150d24832d291e8fbbe
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:26:06 -0700

    Upload code

---
+++ 1022/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+        def dfs(node, num):
+            if not node:
+                return 0
+            num = (num << 1) | node.val
+            if not node.left and not node.right:
+                return num
+            return dfs(node.left, num) + dfs(node.right, num)
+        return dfs(root, 0)
