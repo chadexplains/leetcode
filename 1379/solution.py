1dc404bfdb1103e185f4d533a0388de3e3ecf5b0
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:34:03 -0700

    Upload code

---
+++ 1379/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        stack = [(original, cloned)]
+        while stack:
+            node1, node2 = stack.pop()
+            if node1 == target:
+                return node2
+            if node1.left:
+                stack.append((node1.left, node2.left))
+            if node1.right:
+                stack.append((node1.right, node2.right))
