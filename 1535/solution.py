4046b98374f53f3261b32b203636dccee882dbcb
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:37:33 -0700

    Upload code

---
+++ 1535/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        max_element = arr[0]
+        count = 0
+        for i in range(1, len(arr)):
+            if arr[i] > max_element:
+                max_element = arr[i]
+                count = 0
+            count += 1
+            if count == k:
+                return max_element
+        return max_element
