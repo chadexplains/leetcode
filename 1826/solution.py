00bfd4c1bf6622ff836a94c837e95b737cae1410
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:43:59 -0700

    Upload code

---
+++ 1826/solution.py
@@ -0,0 +1,14 @@
+def maximumSum(nums):
+    n = len(nums)
+    left = [0] * n
+    right = [0] * n
+    left[0] = nums[0]
+    right[n-1] = nums[n-1]
+    for i in range(1, n):
+        left[i] = max(nums[i], nums[i] + left[i-1])
+    for i in range(n-2, -1, -1):
+        right[i] = max(nums[i], nums[i] + right[i+1])
+    res = max(left)
+    for i in range(1, n-1):
+        res = max(res, left[i-1] + right[i+1])
+    return res
