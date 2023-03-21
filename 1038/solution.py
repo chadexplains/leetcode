081772c216c774b665c986ff7c004c9a4253443d
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:26:30 -0700

    Upload code

---
+++ 1038/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        self.sum = 0
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.right)
+            node.val += self.sum
+            self.sum = node.val
+            dfs(node.left)
+        dfs(root)
+        return root
