5a192127012bbf8d85d5cf9d29853055000c74ee
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 08:43:17 -0700

    Upload code

---
+++ 338/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        res = [0] * (num + 1)
+        for i in range(1, num + 1):
+            res[i] = res[i // 2] + i % 2
+        return res
