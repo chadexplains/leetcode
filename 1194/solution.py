1dc498e820042ae258cfb84531a402f7ae2d0158
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 09:29:54 -0700

    Upload code

---
+++ 1194/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+        q = [(0, 0, 0)]
+        visited = set()
+        while q:
+            i, j, steps = q.pop(0)
+            if (i, j) == (x, y):
+                return steps
+            if (i, j) in visited:
+                continue
+            visited.add((i, j))
+            for di, dj in [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]:
+                q.append((i+di, j+dj, steps+1))
