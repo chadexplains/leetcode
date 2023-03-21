f5a0b139b124fab4e0c70f426bd82ce877533382
Chad Explains <123703634+chadexplains@users.noreply.github.com>
2023-03-21 08:44:51 -0700

    Upload code

---
+++ 407/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        if not heightMap:
+            return 0
+        m, n = len(heightMap), len(heightMap[0])
+        heap = []
+        visited = [[False] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                    heap.append((heightMap[i][j], i, j))
+                    visited[i][j] = True
+        heapq.heapify(heap)
+        res = 0
+        while heap:
+            h, i, j = heapq.heappop(heap)
+            for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if 0 <= x < m and 0 <= y < n and not visited[x][y]:
+                    res += max(0, h - heightMap[x][y])
+                    heapq.heappush(heap, (max(heightMap[x][y], h), x, y))
+                    visited[x][y] = True
+        return res
